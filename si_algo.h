cafb2d25db4c56dab60e01362524826564d9cfa3
shufenbuguadi <wogeshidabenxiong@gmail.com>
2018-11-01 01:07:21 +0800

    implement si_list.h, but not debug

--- si_algo.h
+++ si_algo.h
@@ -4,18 +4,20 @@
 // 基础算法，包含全局构造函数，拷贝函数
 #include <cstring>
 #include <new>
+#include "si_traits.h"
+#include "si_iterator.h"
 
 // 填充函数
 // TODO
-template<typename InputIterator, typename OutputIterator>
-void fill(InputIterator& start, InputIterator& end, OutputIterator rst) {
+template<typename ForwardIterator, typename OutputIterator>
+void fill(ForwardIterator& start, ForwardIterator& end, OutputIterator rst) {
     for (; start != end; ++start, ++rst)
         *rst = *start;
 }
 
 // 广义构造函数
 template<typename T1, typename T2>
-void construct(T1* pointer, T2& value) {
+void construct(T1* pointer, const T2& value) {
     new (pointer) T2(value);
 }
 
@@ -23,6 +25,23 @@ void construct(T1* pointer, T2& value) {
 template<typename T>
 void destroy(T* pointer) {
     pointer->~T ();
+}
+
+void destroy(ForwardIterator first, ForwardIterator last) {
+    typedef typename value_type(first) value_type;
+    typedef typename __type_traits<value_type>::has_trivial_destructor __POD_Type;
+    destroy_aux(first, last, __POD_Type());
+}
+
+// do nothing
+void destroy_aux(ForwardIterator first, ForwardIterator last, __POD_True) {
+
+}
+
+// call destructor
+void destroy_aux(ForwardIterator first, ForwardIterator last, __POD_Flase) {
+    for(; first != last; ++first)
+        destroy(&*first);
 }
 
 #endif
